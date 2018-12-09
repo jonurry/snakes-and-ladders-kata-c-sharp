ad08b696701dc082406fbaeb0bd489251efa936e
Jon Urry <jon@urry.me>
2018-12-09 15:31:45 +0000

    Player wins when token lands on 100
    Given the token is on square 97
    When the token is moved 3 spaces
    Then the token is on square 100
    And the player has won the game

--- snakes-ladders/Game.cs
+++ snakes-ladders/Game.cs
@@ -4,14 +4,21 @@ namespace SnakesLadders
 {
   public class Game
   {
+    private bool isWon;
     private int position;
     public Game()
     {
       position = 1;
+      isWon = false;
+    }
+    public bool IsWon
+    {
+      get => isWon;
     }
     public void Move(int spaces)
     {
       position += spaces;
+      if (position == 100) { isWon = true; }
     }
     public int Position
     {
--- tests/PlayerCanWinTheGame.cs
+++ tests/PlayerCanWinTheGame.cs
@@ -11,15 +11,20 @@ namespace SnakesLadders.UnitTests.MovingYourToken
     public Game_PlayerCanWinTheGame()
     {
       _game = new Game();
+      _game.Position = 97;
+      _game.Move(3);
     }
 
     [Fact]
     public void TokenLandsOn100()
     {
-      _game.Position = 97;
-      _game.Move(3);
       Assert.True(_game.Position == 100, "Token did not land on 100");
     }
 
+    [Fact]
+    public void GameIsWonWhenTokenLandsOn100()
+    {
+      Assert.True(_game.IsWon, "Game was not won");
+    }
   }
 }
