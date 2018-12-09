8a8482e3d4ebb735dc0bd2d987f138ec694a233d
Jon Urry <jon@urry.me>
2018-12-09 10:53:07 +0000

    Initial setup of C# command line app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakes_and_ladders_kata_c_sharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
