9e81dfae6be266365c3fdc8d8812cbd9ccc775aa
Jon Urry <jon@urry.me>
2018-12-09 15:08:43 +0000

    Token Moves Are Incremental
    Given the token is on square 1
    When the token is moved 3 spaces
    And then it is moved 4 spaces
    Then the token is on square 8

--- tests/MovingYourToken.cs
+++ tests/MovingYourToken.cs
@@ -16,14 +16,22 @@ namespace SnakesLadders.UnitTests
     [Fact]
     public void TokenStartsOnSquare1()
     {
-      Assert.True(_game.Position == 1, "1 should not be prime");
+      Assert.True(_game.Position == 1, "Token does not start on square 1");
     }
 
     [Fact]
     public void TokenMoves3Spaces()
     {
       _game.Move(3);
-      Assert.True(_game.Position == 4, "1 should not be prime");
+      Assert.True(_game.Position == 4, "Token does not move 3 spaces");
+    }
+
+    [Fact]
+    public void TokenMovesAreIncremental()
+    {
+      _game.Move(3);
+      _game.Move(4);
+      Assert.True(_game.Position == 8, "Token moves are not incremental");
     }
   }
 }
